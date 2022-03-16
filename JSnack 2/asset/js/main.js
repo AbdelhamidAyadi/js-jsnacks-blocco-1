da00a6d4c4a55ca4fc28d6fa0deaf5a1dc459024
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-03-16 12:21:32 +0100

    Esercizio 2 completato.

---
+++ JSnack 2/asset/js/main.js
@@ -0,0 +1,15 @@
+let container = document.getElementById("container");
+let firstWord = prompt("Inserisci la prima parola:");
+let secondWord = prompt("Inserisci la seconda parola:");
+
+console.log(firstWord);
+console.log(secondWord);
+
+if(firstWord.length > secondWord.length){
+    container.innerHTML = `<div>${firstWord}</div>`;
+
+}else if( firstWord.length < secondWord.length ){
+    container.innerHTML = `<div>${secondWord}</div>`;
+}else{
+    container.innerHTML = `<div>Le due parole hanno lo stesso numero di lettere</div>`;
+}
